edf9dbfe907d79cfa8a69e1af4f04586463ba13c
Patrick Arlt <patrick.arlt@gmail.com>
2014-02-26 19:33:06 -0800

    rewrite layer fetching and building

--- tasks/tasks.js
+++ tasks/tasks.js
@@ -47,7 +47,7 @@ module.exports = function (grunt) {
       if (!item.title.match('Mature Support')) {
         layers.push({
           country: country,
-          hash: country.replace(/\s/g, '-').replace(/\W+/g, ' ').toLowerCase(),
+          hash: country.replace(/ /g, '-').replace(/\W+/g, '').toLowerCase(),
           filename: country.replace(/ /g, '-').toLowerCase(),
           key: item.title.replace(/\W/g, ''),
           id: item.id
